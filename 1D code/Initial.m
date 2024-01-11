374de8fb0a565926f80eb726f0c11263a6c7acd9
Shayne-Falco <plourde.5@osu.edu>
2024-01-11 14:41:01 -0500

    delete to update

--- 1D code/Initial.m
+++
@@ -1,3 +0,0 @@
-function I=Initial(Nx,spikes,L,A)
-k=linspace(0,2*L,Nx);
-I=.005.*sin(k.*(pi*spikes/L))+A;
--- 1D code/forcing.m
+++
@@ -1,4 +0,0 @@
-function I=forcing(Nx,spikes,L,A)
-k=linspace(0,2*L,Nx);
-I=A.*sin(k.*(pi*spikes/L));
-I(I<0)=0;
